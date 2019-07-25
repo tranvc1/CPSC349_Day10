b4d712cb234296ba19b2b9c1396f7c622f230e4d
Vince <vince.project.design@gmail.com>
2019-07-24 19:43:32 -0700

    JSON function start

--- day10/comments.js
+++ day10/comments.js
@@ -1,26 +1,39 @@
-(function (window) {
-  'use strict';
+(function(window) {
+  "use strict";
 
   const BUTTON_SELECTOR = '[data-posts="id"]';
 
   let buttons = document.querySelectorAll(BUTTON_SELECTOR);
 
-  buttons.forEach(function (button) {
-    'use strict';
+  buttons.forEach(function(button) {
+    "use strict";
 
     let sectionSelector = `#comments-${button.value}`;
     let commentSection = document.querySelector(sectionSelector);
 
-    button.addEventListener('click', function (event) {
+    button.addEventListener("click", function(event) {
       if (commentSection.hidden) {
         commentSection.hidden = false;
-        button.textContent = 'Hide comments';
+        button.textContent = "Hide comments";
       } else {
         commentSection.hidden = true;
-        button.textContent = 'Show comments';
+        button.textContent = "Show comments";
       }
 
       event.preventDefault();
     });
   });
 })(window);
+
+fetch("http://jsonplaceholder.typicode.com/posts")
+  .then(response => response.json())
+  .then(json => {
+    let articleArray = json;
+    articleArray.forEach(function(element) {
+      let articleX = document.createElement("article");
+      articleX.innerHTML = "text";
+      document.body.appendChild(articleX);
+      console.log(element);
+    });
+    //console.log(json);
+  });
